b5573af533d1267336b735253c03b0baa2b0b72c
RussellRWG <russellrwg@gmail.com>
2026-01-05 17:20:02 -0500

    Adjusting Tauri config + Hello World Rust function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { invoke } from "@tauri-apps/api/core";
+
+    let message: string = "Loading...";
+
+    onMount(() => {
+        (async () => {
+            try {
+                message = await invoke<string>("hello_world");
+            } catch (err) {
+                console.error(err);
+                message = "Error calling Rust (see console)";
+            }
+        })();
+    });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
+<h2>{message}</h2>
